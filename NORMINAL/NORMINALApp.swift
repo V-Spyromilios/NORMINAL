29f8d67014c50c4452c70b43ee1b2b60f122c719
Evangelos Spyromilios <71439594+V-Spyromilios@users.noreply.github.com>
2022-09-21 13:02:06 +0200

    Initial Commit

---
+++ NORMINAL/NORMINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  NORMINALApp.swift
+//  NORMINAL
+//
+//  Created by Evangelos Spyromilios on 21.09.22.
+//
+
+import SwiftUI
+
+@main
+struct NORMINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
